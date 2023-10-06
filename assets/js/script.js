836417f980ec6239fd71086b47b0fbfeec0812b1
abdullatif <abdullatifjr002@gmail.com>
2023-10-06 20:21:51 +0300

    bug fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,27 +4,55 @@ let cart = document.querySelector(".cart");
 let shoppingCart = document.querySelector(".shopping-cart");
 let cartOverlay = document.querySelector(".cart-overlay");
 
-function disableScrolling() {
-  var x = window.scrollX;
-  var y = window.scrollY;
-  window.onscroll = function () {
-    window.scrollTo(x, y);
-  };
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e.preventDefault();
 }
 
-function enableScrolling() {
-  window.onscroll = function () {};
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
 }
 
 shoppingCart.addEventListener("click", () => {
   cart.classList.add("cart-shown");
   cartOverlay.classList.add("overlay-shown");
-  disableScrolling();
+  disableScroll();
 });
 closeBtn.addEventListener("click", () => {
   cart.classList.remove("cart-shown");
   cartOverlay.classList.remove("overlay-shown");
-  enableScrolling();
+  enableScroll();
 });
 
 // Flags List API
